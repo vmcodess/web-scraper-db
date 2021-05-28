667403c2394a92e67dd5e6b4b10ef7c4166e4576
vmatveev <vmatveevspam@gmail.com>
2021-05-27 20:59:37 -0400

    removed unecessary comments

--- server.js
+++ server.js
@@ -13,25 +13,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(cors());
 
-// const database = {
-//   users: [
-//     {
-//       id: '123',
-//       name: 'John',
-//       email: 'john@gmail.com',
-//       password: 'cookies',
-//       joined: new Date()
-//     },
-//     {
-//       id: '1234',
-//       name: 'Sally',
-//       email: 'sally@gmail.com',
-//       password: 'bananas',
-//       joined: new Date()
-//     }
-//   ]
-// }
-
 
 // -----------------------------------------------------------------------------------------MongoDB
 mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -46,7 +27,6 @@ mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTop
 
 // ----- ROUTES ----- \\
 app.get('/', (req, res) => {
-  //res.send(database.users)
   res.send('work!')
 })
 
@@ -65,8 +45,6 @@ app.post('/signin', (req, res) => {
   .catch((err) => {
     console.log('error comparing passwords', err)
   })
-
-  //res.json('working')
 })
 
 app.post('/register', (req,res) => {
@@ -78,22 +56,6 @@ app.post('/register', (req,res) => {
 
   const user = userModel(newUser);
 
-  // check if user exists..
-  // newUser.find({ email: newUser.email })
-  // .then((user) => {
-  //   console.log('User does not exist:', user);
-  //   user.save()
-  //   .then((user) => {
-  //     console.log('User saved to DB: ', user);
-  //   })
-  //   .catch((err) => {
-  //     console.log('User Could not be saved to DB: ', err)
-  //   })
-  // })
-  // .catch((err) => {
-  //   console.log('User already exists: ', err);
-  // })
-
   user.save()
   .then((user) => {
     console.log('User Successfully saved to DB!!', user);
@@ -102,20 +64,9 @@ app.post('/register', (req,res) => {
     console.log('Error saving user to DB', err)
   })
 
-  
-    
-    // else (if doesnt exist)
-      // save to databse
-
 })
 
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
-
-/* 
- - signin --> POST success/fail
- - register --> POST = user
- - profile/:userId --> GET = user
-*/
